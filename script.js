8f546883c13aacfc630373eeb31f468bdfe9eb07
Derrick <derrick.chow234@gmail.com>
2020-06-01 14:02:00 -0400

    Added third question

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const firstElem = document.getElementById("container");
 //Question 2 
 const secondElem = document.querySelector("#container");
 
+//Question 3 
+const thirdElem = document.getElementsByClassName("second");
