3248458ce4968dc05ca03035532956d1d27b920d
Temurbek Sabirov <prodota150@gmail.com>
2020-06-01 14:46:45 -0400

    Add li element

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //Question 1
 const firstElem = document.getElementById("container");
 
-//Question 2 
+//Question 2
 const secondElem = document.querySelector("#container");
 
-//Question 3 
+//Question 3
 const thirdElem = document.getElementsByClassName("second");
+
+const li = document.createElement("LI");
+const textnode = document.createTextNode("four");
+li.appendChild(textnode);
+ul.appendChild(li);
